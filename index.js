94b9b8c29a209cc8e81fb98a9ba1ca30466f035e
Winnie Wang <winniewang@winnies-air.wifi.int.ryerson.ca>
2019-03-26 19:32:52 -0400

    added the database

--- index.js
+++ index.js
@@ -8,53 +8,61 @@ mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true}
 app.use(badyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-var db = mongoose.connection;
+//define a pattern for the documents
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String,
+});
 
-    //define a pattern for the documents
-    var campgroundSchema = new mongoose.Schema({
-        name: String,
-        image: String,
-
-    });
-
-    Campground = mongoose.model('Campground', campgroundSchema);
-    // Campground.create(
-    //     {
-    //         name: "Second Ground",
-    //         image: "https://farm4.staticflickr.com/3211/3062207412_03acc28b80.jpg"
-    //     }, function(err, campground){
-    //         if(err){
-    //             console.log("ERROR");
-    //         }else{
-    //             console.log('Newly created campground');
-    //             console.log(campground);
-    //         }
-    //     });
-
-
-
+Campground = mongoose.model('Campground', campgroundSchema);
+        //
+        // Campground.create(
+        //     {
+        //         name: "Second Ground",
+        //         image: "https://farm4.staticflickr.com/3211/3062207412_03acc28b80.jpg",
+        //         description: "This is a huge hill",
+        //
+        //     }, function(err, campground){
+        //         if(err){
+        //             console.log("ERROR");
+        //         }else{
+        //             console.log('Newly created campground');
+        //             console.log(campground);
+        //         }
+        //     });
 
 app.get("/", function(req, res){
     res.render("landing");
 });
 
+//INDEX
 app.get("/campgrounds", function(req, res){
         Campground.find({}, function(err, allCampgrounds){
             if(err) {
                 console.log("ERROR");
             }else{
-                res.render("campgrounds", {campgrounds: allCampgrounds});
+                res.render("index", {campgrounds: allCampgrounds});
             }
         });
 });
 
+//NEW
+app.get("/campgrounds/new", function(req, res){
+    //find the template with that campground
+    res.render("new");
+});
+
+//CREAT
 app.post("/campgrounds", function(req, res){
-    var name= req.body.name;
-    var img = req.body.image;
+    console.log(req.body.description);
+
     Campground.create(
         {
-            name: name,
-            image: img,
+            name: req.body.name,
+            image: req.body.image,
+            description: req.body.description
+
         }, function(err, campground){
             if(err){
                 console.log("ERROR");
@@ -65,8 +73,18 @@ app.post("/campgrounds", function(req, res){
 
 });
 
-app.get("/campgrounds/new", function(req, res){
-    res.render("new");
+
+
+//SHOW, must be decalred after /new
+app.get("/campgrounds/:id", function(req, res){
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err){
+            console.error.bind(console, "error");
+        }else{
+            res.render("show", {campground: foundCampground});
+        }
+
+    });
 });
 
 app.listen(3000, function(){
